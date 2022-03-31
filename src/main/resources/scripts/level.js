baa3b3699a376cc7852b48e634048f13140dba71
cdc12345 <3154934427@qq.com>
2022-03-31 13:13:46 +0800

    经验公式

--- src/main/resources/scripts/level.js
+++ src/main/resources/scripts/level.js
@@ -1,9 +1,7 @@
---lua编写的
-lv = player.getLevel()
-if (lv < 100) then
-    exp = (50/3)*lv^3-(25/2)*lv^2-(25/6)*lv
-else
-    -- -1代表永远都不会升级 ，除此之外的负数无法抑制升级
-    exp = -1
-end
-    player.setMaxExp(exp)
+//经验计算
+val level = mob.getLevel();
+if (level == 100){
+    mob.setMaxExp(-1);
+} else {
+    mob.setMaxExp(20*Math.pow(10,level));
+}
